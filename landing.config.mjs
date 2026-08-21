b9aeed66214c13708c1e6288b74f29d77a208dff
Shamber <147848650+shambeer@users.noreply.github.com>
2026-08-21 13:15:57 +0300

    marking done

---
+++ landing.config.mjs
@@ -0,0 +1,17 @@
+export default {
+  schemaVersion: 2,
+  builder: {
+    minVersion: "2.9.0",
+  },
+  project: {
+    name: "sunmar-ny-2027",
+  },
+  site: {
+    preset: "sunmar",
+  },
+  stack: {
+    script: "js",
+    markup: "html",
+    styles: "scss",
+  },
+};
